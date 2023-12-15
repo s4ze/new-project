2a40c3a37cc1c527452f70135c9227d2e9d0aad1
s4ze <imgood1104@gmail.com>
2023-12-15 18:45:01 +0500

    debug-2 fix

--- debugging.cs
+++ debugging.cs
@@ -4,6 +4,7 @@ int b = 20;
 Console.WriteLine('введите значение числа a');
 Console.Readline(a);
 <<<<<<< HEAD
+<<<<<<< HEAD
 Console.WriteLine(b);
 a += 10;
 b += 10;
@@ -11,4 +12,7 @@ Console.WriteLine(b);
 =======
 a += 25;
 >>>>>>> 14b2b48 (adding text in debug-2)
+=======
+a += 50;
+>>>>>>> 0ea0c0e (debug-2 fix)
 Console.WriteLine('новое значение числа a:', a);
