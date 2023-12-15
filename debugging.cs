0ea0c0ecfd0d693dccfef45a5903463d953bdce3
s4ze <imgood1104@gmail.com>
2023-12-15 18:45:01 +0500

    debug-2 fix

--- debugging.cs
+++ debugging.cs
@@ -2,5 +2,5 @@ Console.WriteLine('файл для дебага');
 int a;
 Console.WriteLine('введите значение числа a');
 Console.Readline(a);
-a += 25;
+a += 50;
 Console.WriteLine('новое значение числа a:', a);
