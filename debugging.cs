6f74d2c5d3d68c9906316fb5e971695cee9ed33e
s4ze <imgood1104@gmail.com>
2023-12-15 18:44:15 +0500

    adding text in debug-2

--- debugging.cs
+++ debugging.cs
@@ -3,8 +3,12 @@ int a = 0;
 int b = 20;
 Console.WriteLine('введите значение числа a');
 Console.Readline(a);
+<<<<<<< HEAD
 Console.WriteLine(b);
 a += 10;
 b += 10;
 Console.WriteLine(b);
+=======
+a += 25;
+>>>>>>> 14b2b48 (adding text in debug-2)
 Console.WriteLine('новое значение числа a:', a);
