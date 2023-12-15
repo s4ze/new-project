e098b8af424f0a7bb8774401e94d838104b839ca
s4ze <imgood1104@gmail.com>
2023-12-15 18:46:35 +0500

    adding int b

--- debugging.cs
+++ debugging.cs
@@ -5,6 +5,7 @@ Console.WriteLine('введите значение числа a');
 Console.Readline(a);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 Console.WriteLine(b);
 a += 10;
 b += 10;
@@ -15,4 +16,12 @@ a += 25;
 =======
 a += 50;
 >>>>>>> 0ea0c0e (debug-2 fix)
+=======
+a += 50; s
+Console.WriteLine(b);
+a += 10;
+b += 10;
+int c;
+Console.WriteLine(b);
+>>>>>>> 87470cf (adding int b)
 Console.WriteLine('новое значение числа a:', a);
