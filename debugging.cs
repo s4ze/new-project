1b10395933941537905f1f45e027cf945600b904
s4ze <imgood1104@gmail.com>
2023-12-15 19:44:00 +0500

    fixing git stuff

--- debugging.cs
+++ debugging.cs
@@ -3,29 +3,17 @@ int a = 0;
 int b = 20;
 Console.WriteLine('введите значение числа a');
 Console.Readline(a);
-<<<<<<< HEAD
 a += 50;
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 Console.WriteLine(b);
 a += 10;
 b += 10;
 Console.WriteLine(b);
-=======
 a += 25;
->>>>>>> 14b2b48 (adding text in debug-2)
-=======
 a += 50;
->>>>>>> 0ea0c0e (debug-2 fix)
-=======
 a += 50; s
 Console.WriteLine(b);
 a += 10;
 b += 10;
 int c;
 Console.WriteLine(b);
->>>>>>> 87470cf (adding int b)
->>>>>>> e098b8af424f0a7bb8774401e94d838104b839ca
 Console.WriteLine('новое значение числа a:', a);
